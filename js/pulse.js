858dcc7244550fccd29c8f8a0d2d90302eb6f327
SuleymanovTahir <90999791+SuleymanovTahir@users.noreply.github.com>
2021-12-16 03:20:53 +0300

    first commit

---
+++ js/pulse.js
@@ -0,0 +1,19 @@
+﻿/*
+Скрипт для точек на кресле
+*/
+
+function hoverOnfirst () {
+    document.getElementById('adv_list_item1').classList.add('active')
+}
+
+function hoverOfffirst () {
+    document.getElementById('adv_list_item1').classList.remove('active')
+}
+
+function hoverOnfirst () {
+    document.getElementById('pulse').classList.add('active')
+}
+
+function hoverOfffirst () {
+    document.getElementById('pulse').classList.remove('active')
+}
---
+++ js/slick.option.js
@@ -0,0 +1,32 @@
+﻿
+$(document).ready(function()
+
+{
+    $('.slider_main').slick ({
+        dots: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight:true
+    });
+
+}
+
+)
+,
+
+$(document).ready(function()
+
+{
+    $('.slider_otziv').slick ({
+        dots: false,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 2,
+        adaptiveHeight:true
+    });
+
+}
+
+)
+
